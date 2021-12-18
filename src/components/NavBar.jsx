f75f48b256a0d91aa2ebe69e5348fe3389a59873
Quan <nquan7252@gmail.com>
2021-12-18 14:17:42 -0800

    new

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ function NavBar({data,getNew}){
     return (
         <div style={{display:'block'}}>
         <form onSubmit={getNew} className="d-flex " style={{marginRight:100,marginLeft:100,paddingTop:50}}>
-        <input id="input" className="form-control me-2" type="search" placeholder="Search" aria-label="Search"   style={{background:'transparent',borderColor:'black'}}/>
-        <button className="btn btn-outline-dark"  type="submit">Search</button>
+        <input id="input" className="form-control me-2" type="search" placeholder="Search" aria-label="Search" style={{color:'white',background:'transparent',borderColor:'white'}}/>
+        <button className="btn btn-outline-light"  type="submit">Search</button>
         </form>
   
         </div>
